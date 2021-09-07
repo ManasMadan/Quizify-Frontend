86a4c6c5b3daf80f9facea16fbd6ac2c299ddf6b
Manas <manasmadan08@gmail.com>
2021-09-07 20:16:06 +0530

    Route Fixes

--- src/components/CreateQuizQuestions.js
+++ src/components/CreateQuizQuestions.js
@@ -1,12 +1,20 @@
-import { useParams } from "../base";
+import { useParams, useSelector, useHistory } from "../base";
 
 export default function CreateQuizQuestions() {
+  const history = useHistory();
   const { quizcode } = useParams();
-  return (
-    <div className="container-fluid">
-      <h5 style={{ width: "100vw", textAlign: "center" }}>
-        QuizCode : {quizcode}
-      </h5>
-    </div>
-  );
+  const loggedIn = useSelector((state) => state.changeLoginState);
+
+  if (loggedIn) {
+    return (
+      <div className="container-fluid">
+        <h5 style={{ width: "100vw", textAlign: "center" }}>
+          QuizCode : {quizcode}
+        </h5>
+      </div>
+    );
+  } else {
+    history.push("/");
+    return null;
+  }
 }
--- src/components/JoinQuizQuestions.js
+++ src/components/JoinQuizQuestions.js
@@ -1,10 +1,18 @@
-import { useParams } from "../base";
+import { useParams, useSelector, useHistory } from "../base";
 
 export default function JoinQuizQuestions() {
   const { quizcode } = useParams();
-  return (
-    <div className="container">
-      <h5 style={{ textAlign: "center" }}>QuizCode : {quizcode}</h5>
-    </div>
-  );
+  const loggedIn = useSelector((state) => state.changeLoginState);
+  const history = useHistory();
+
+  if (loggedIn) {
+    return (
+      <div className="container">
+        <h5 style={{ textAlign: "center" }}>QuizCode : {quizcode}</h5>
+      </div>
+    );
+  } else {
+    history.push("/");
+    return null;
+  }
 }
